d59fcbbeaa1dc562314603c01b5665841e82ce6d
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-04-05 06:36:53 -0700

    ecommerce shop

--- src/components/products/CheckoutForm.jsx
+++ src/components/products/CheckoutForm.jsx
@@ -18,7 +18,7 @@ const CheckoutForm = () => {
        const {error} = await stripe.confirmPayment({
         elements,
         confirmParams: {
-            return_url: `http://localhost:3000`
+            return_url: `https://ecommerce-react-stripe.netlify.app`
         }
        })
        if(error) {
